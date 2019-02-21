4bd8b27197b3c780355a83a72ed16e08310ea07e
Guillar1 <coolbam14@gmail.com>
2019-02-20 21:18:18 -0800

    Added movement

---
+++ Overseasoned/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody playerRigidbody;
+
+    public float Speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movementModifierX = Input.GetAxisRaw("Horizontal");
+        float movementModifierZ = Input.GetAxisRaw("Vertical");
+        Vector3 newVelocity = playerRigidbody.velocity;
+        newVelocity.x = movementModifierX * Speed;
+        newVelocity.z = movementModifierZ * Speed;
+        playerRigidbody.velocity = newVelocity;
+    }
+}
