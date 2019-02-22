0f3d4734e9dd396f734f15bde3eae766e120a8cf
Marshall Nguyen <marshahn@uci.edu>
2019-02-21 21:46:08 -0800

    Working on movement
    Need  to fix rotation

--- Overseasoned/Assets/Scripts/Player.cs
+++ Overseasoned/Assets/Scripts/Player.cs
@@ -16,11 +16,24 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        checkObject();
         float movementModifierX = Input.GetAxisRaw("Horizontal");
         float movementModifierZ = Input.GetAxisRaw("Vertical");
+
+     
         Vector3 newVelocity = playerRigidbody.velocity;
         newVelocity.x = movementModifierX * Speed;
         newVelocity.z = movementModifierZ * Speed;
         playerRigidbody.velocity = newVelocity;
+    }
+
+    void checkObject()
+    {
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        Vector3 rayCastPos = new Vector3(transform.position.x, transform.position.y + 2, transform.position.z);
+        Debug.DrawRay(transform.position, fwd, Color.green);
+        bool raycasthit = Physics.Raycast(rayCastPos, fwd, 50);
+
+
     }
 }
