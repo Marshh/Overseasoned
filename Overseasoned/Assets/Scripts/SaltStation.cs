f937188cd57677b0d46e0693237bcefad659b74e
Marshh <nguyenmarshall123@gmail.com>
2019-02-25 20:39:02 -0800

    SaltStation instantiation
    Spawns a salt cylinder for now

--- Overseasoned/Assets/Scripts/SaltStation.cs
+++ Overseasoned/Assets/Scripts/SaltStation.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class SaltStation : MonoBehaviour
 {
-
+    public GameObject Salt;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnSalt();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void spawnSalt()
+    {
+        Vector3 unitAbove = this.transform.position + Vector3.up;
+
+        Instantiate(Salt, unitAbove, Quaternion.identity);
     }
 }
