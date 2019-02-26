3888c15ef5ae2e92a0f852835c9f8bdabd095301
Guillar1 <coolbam14@gmail.com>
2019-02-26 12:51:24 -0800

    Made the saltstation a spicestation. Takes in any type of spice now. Also respawns the spice once taken.

--- Overseasoned/Assets/Scripts/Player.cs
+++ Overseasoned/Assets/Scripts/Player.cs
@@ -61,8 +61,9 @@ public class Player : MonoBehaviour
         {
             if (hit.collider.CompareTag("Salt"))
             {
-                Debug.Log("Salty");
-                hit.collider.gameObject.SendMessage("OnPickup", hit.collider);
+//                Debug.Log("Salty");
+//                hit.collider.gameObject.SendMessage("OnPickup", hit.collider);
+                Destroy(hit.collider.gameObject);
             }
 //            Debug.Log(hit.distance);
         }
