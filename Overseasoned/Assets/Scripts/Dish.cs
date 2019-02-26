5ba440aff90defee9215e77a8b758c3fa3fd4aa8
Marshh <nguyenmarshall123@gmail.com>
2019-02-25 21:29:32 -0800

    Added dish script
    has a function that checks for curry consituents

---
+++ Overseasoned/Assets/Scripts/Dish.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dish : MonoBehaviour
+{
+    public int Spiciness;
+
+    public List<string> ingredients;
+    // Start is called before the first frame update
+    void awake()
+    {
+        Spiciness = 0;
+        ingredients = new List<string> { };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void addIngredient(string ingredient)
+    {
+        ingredients.Add(ingredient);
+    }
+
+    int spicinessCheck()
+    {
+        return Spiciness;
+    }
+
+
+    bool checkCurry()
+    {
+        List<string> curryIngredients = new List<string> {"Rice", "Curry"};
+        bool hasAll = true;
+        for (int i = 0; i < curryIngredients.Count; ++i)
+        {
+            bool found = false;
+            for(int j = 0; j < ingredients.Size; ++j)
+            {
+                if (curryIngredients[i] == ingredients[j])
+                {
+                    found = true;
+                }
+            }
+
+            if( !found )
+            {
+                hasAll = false;
+            }
+
+        }
+        return hasAll;
+    }
+}
