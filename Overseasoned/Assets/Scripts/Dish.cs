a9b2ebf7a2e758cca4d9e023567c467bb1309657
Guillar1 <coolbam14@gmail.com>
2019-03-01 18:54:01 -0800

    Can pick up plate now. Made movement a bit less jittery using fixedupdate.

--- Overseasoned/Assets/Scripts/Dish.cs
+++ Overseasoned/Assets/Scripts/Dish.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Dish : MonoBehaviour
 {
     public int Spiciness;
-    public bool hasAll = false;
+//    public bool hasAll = false;
     public List<string> ingredients;
     // Start is called before the first frame update
     void awake()
@@ -33,36 +33,36 @@ public class Dish : MonoBehaviour
 
     public void clear()
     {
-        hasAll = false;
+//        hasAll = false;
         ingredients.Clear();
     }
-    public void checkCurry()
-    {
-        List<string> curryIngredients = new List<string> {"Rice", "Curry"};
-        //        hasAll = true;
-        //        for (int i = 0; i < curryIngredients.Count; ++i)
-        //        {
-        //            bool found = false;
-        //            for(int j = 0; j < ingredients.Count; ++j)
-        //            {
-        //                if (curryIngredients[i] == ingredients[j])
-        //                {
-        //                    found = true;
-        //                }
-        //            }
-        //
-        //            if( !found )
-        //            {
-        //                hasAll = false;
-        //            }
-        //
-        //        }
-        if (curryIngredients.OrderBy(x => x).SequenceEqual(ingredients.OrderBy(x => x)))
-        {
-            hasAll = true;
-
-        }
-        
-
-    }
+//    public void checkCurry()
+//    {
+//        List<string> curryIngredients = new List<string> {"Rice", "Curry"};
+//        //        hasAll = true;
+//        //        for (int i = 0; i < curryIngredients.Count; ++i)
+//        //        {
+//        //            bool found = false;
+//        //            for(int j = 0; j < ingredients.Count; ++j)
+//        //            {
+//        //                if (curryIngredients[i] == ingredients[j])
+//        //                {
+//        //                    found = true;
+//        //                }
+//        //            }
+//        //
+//        //            if( !found )
+//        //            {
+//        //                hasAll = false;
+//        //            }
+//        //
+//        //        }
+//        if (curryIngredients.OrderBy(x => x).SequenceEqual(ingredients.OrderBy(x => x)))
+//        {
+//            hasAll = true;
+//
+//        }
+//        
+//
+//    }
 }
--- Overseasoned/Assets/Scripts/Player.cs
+++ Overseasoned/Assets/Scripts/Player.cs
@@ -15,20 +15,17 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        IniDish();
+//        IniDish();
     }
 
-    void Update()
-    {
-        Movement();
-    }
-
+   
     void FixedUpdate()
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
             checkObject();
         }
+        Movement();
 
     }
 
@@ -52,36 +49,46 @@ public class Player : MonoBehaviour
     void checkObject()
     {
 
-        // Vector3 fwd = transform.TransformDirection(Vector3.forward);
-        // Vector3 rayCastPos = new Vector3(transform.position.x, transform.position.y + 2, transform.position.z);
-        // Debug.DrawRay(transform.position, fwd, Color.green);
-        // bool raycasthit = Physics.Raycast(rayCastPos, fwd, 50);
-
-
-        //        Vector3 rayCastPos = new Vector3(transform.position.x, transform.position.y + 2, transform.position.z);
-
-        //       Ray ray = new Ray(transform.position+new Vector3(0,.25f,0), fwd);
+       
 
         //Ignore dish infront of player
         int layerMask = 1 << 8;
         layerMask = ~layerMask;
 
-
         Vector3 fwd = transform.TransformDirection(new Vector3(0, 0, 5));
-        Debug.DrawRay(transform.position + new Vector3(0, .5f, 0), fwd, Color.green);
+        //Debug.DrawRay(transform.position + new Vector3(0, .5f, 0), fwd, Color.green);
+        Debug.DrawRay(transform.position + new Vector3(0, -.25f, 0),fwd);
         //The sphere can't spawn on the item collider. It won't detect it.
         float thickness = .30f;
-        if(Physics.SphereCast(transform.position+ new Vector3(0, .5f, 0), thickness, fwd, out RaycastHit hit,2f,layerMask))
+        RaycastHit hit;
+        if (Physics.SphereCast(transform.position+ new Vector3(0, .5f, 0), thickness, fwd, out hit,2f,layerMask))
         {
             if (hit.collider.CompareTag("Spice"))
             {
-                //                Debug.Log("Salty");
+                //                Debug.Logwwssd("Salty");
                 //                hit.collider.gameObject.SendMessage("OnPickup", hit.collider);
                 print(hit.collider.gameObject.name);
                 dish.GetComponent<Dish>().addIngredient(hit.collider.gameObject.name);
                 Destroy(hit.collider.gameObject);
             }
-//            Debug.Log(hit.distance);
+            else 
+            //            Debug.Log(hit.distance);
+            print(hit.distance);
+        }
+
+        if (Physics.Raycast(transform.position + new Vector3(0, -.25f, 0), fwd, out hit, 2f))
+        {
+            if (hit.collider.CompareTag("Station"))
+            {
+                //Pick up plate
+                dish = hit.collider.gameObject.GetComponent<DishStation>().getDish();
+                dish.GetComponent<Rigidbody>().detectCollisions = true;
+                dish.GetComponent<Rigidbody>().useGravity = false;
+                dish.GetComponent<Rigidbody>().isKinematic = true;
+                dish.transform.SetParent(transform);
+                dish.transform.localPosition = new Vector3(0, 0, 1);
+                
+            }
         }
     }
 
