f2246b3bdf5ec07e8aafc40dfcc629b04bae3299
Marshh <nguyenmarshall123@gmail.com>
2019-03-04 00:37:50 -0800

    Added trash can
    the trash can deletes the dish and everything on it

---
+++ Overseasoned/Assets/Scripts/Trash.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trash : MonoBehaviour
+{
+    public void deleteDish(Transform player)
+    {
+        foreach (Transform child in player.transform)
+        {
+            if (!(child.name == "ScaleReset"))
+            {
+                Destroy(child.gameObject);
+            }
+        }
+    }
+}
