98a33669417c5a8457c6756f4a95fa30ef667f81
Kunal <kunal@Kunals-MacBook-Pro.local>
2019-03-04 23:17:11 -0800

    Added Main Menu and End Menu

---
+++ Overseasoned/Assets/Scripts/EndMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class EndMenu : MonoBehaviour
+{
+    public void loadGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}
