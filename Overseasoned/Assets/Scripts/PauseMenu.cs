e747bbdba4c6b1f610b275c6d154df7e9b628267
Marshall Nguyen <marshahn@uci.edu>
2019-03-17 03:24:56 -0700

    Still need to bind pause menu buttons

---
+++ Overseasoned/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+
+    public GameObject pauseUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( Input.GetKeyDown(KeyCode.Escape) )
+        {
+            if ( gameIsPaused )
+            {
+                resume();
+            }
+            else
+            {
+                pause();
+            }
+        }
+    }
+
+    void resume()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void pause()
+    {
+        pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+}
